f6708884cb68b366dceb92047b33ad9ba71bedd4
ysj0227 <1976290043@qq.com>
2019-12-17 17:39:09 +0800

    Update AppConfig.java

--- app/src/main/java/com/finance/biiid/config/AppConfig.java
+++ app/src/main/java/com/finance/biiid/config/AppConfig.java
@@ -1,23 +1,6 @@
 package com.finance.biiid.config;
 
 public class AppConfig {
-
-    /**
-     * 测试地址
-     */
-//    public final static String URL_TEST = "https://pai.qianyusoft.cn/ddq_front/entrance.html";
-//    public final static String URL_TEST = "https://pai.qianyusoft.cn/ddq_app/entrance.html";
-    /**
-    /*
-     * 线上地址
-     */
-//    public final static String URL_RELEASE = "https://wei.bidddq.com/ddq_front/entrance.html";
-    
-    /**
-     * 查询支付订单
-     */
-//    public final static String QUERY_TRADE_STATUS = "https://pai.qianyusoft.cn/ddq/appQueryWxOrder";
-
     /**
      * 微信分享登录
      */
@@ -29,4 +12,6 @@ public class AppConfig {
     public final static int WX_TYPE_SEND_IMG=3;
     public final static int WX_TYPE_SEND_QR_IMG=4;
 
+    //base64图片内容
+    public static String base64ImgData;
 }
